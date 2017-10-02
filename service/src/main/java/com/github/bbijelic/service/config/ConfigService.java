9112fae7f48e9b2adb7beaef7632d32ac1019143
Bojan Bijelic <bojan.bijelic.os@gmail.com>
2017-10-02 15:35:01 +0000

    main class initial impl

--- service/src/main/java/com/github/bbijelic/service/config/ConfigService.java
+++ service/src/main/java/com/github/bbijelic/service/config/ConfigService.java
@@ -1,14 +1,68 @@
 package com.github.bbijelic.service.config;
 
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.bbijelic.service.config.config.ServiceConfiguration;
+
+import io.dropwizard.Application;
+import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
+import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
 /**
  * Configuration service main class
  * 
  * @author Bojan Bijelic
  */
-public class ConfigService {
+public class ConfigService extends Application<ServiceConfiguration> {
     
-    public static void main(String[] args) {
-        
+    /**
+     * Logger
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigService.class);
+    
+    /**
+     * Main
+     * 
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        new ConfigService().run(args);
     }
     
+    /**
+     * Service instance uuid
+     */
+    private UUID serviceInstanceUUID;
+    
+    /**
+     * Constructor
+     */
+    public ConfigService() {
+        serviceInstanceUUID = UUID.randomUUID();
+    }
+    
+    @Override
+    public String getName() {
+        return "config-service-" + serviceInstanceUUID.toString();
+    }
+        
+    @Override
+    public void initialize(Bootstrap<ServiceConfiguration> bootstrap) {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+            new SubstitutingSourceProvider(
+                bootstrap.getConfigurationSourceProvider(),
+                new EnvironmentVariableSubstitutor(false)));
+    }
+    
+    @Override
+    public void run(ServiceConfiguration arg0, Environment arg1) throws Exception {
+        LOGGER.info("Starting " + getName());
+        
+    }
 }
---
+++ service/src/main/java/com/github/bbijelic/service/config/config/ServiceConfiguration.java
@@ -0,0 +1,13 @@
+package com.github.bbijelic.service.config.config;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Service configuration class
+ * 
+ * @author Bojan Bijelic
+ */
+public class ServiceConfiguration extends Configuration {
+    
+    
+}
